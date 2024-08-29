6b24a6b8745cba8c917c8de4d9d6b856e015dad1
Abigail Werthmann <abbey5048@gmail.com>
2024-08-29 13:50:54 -0400

    commiting CalcHalf

---
+++ HWs/HW00/CalcHalf/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+double CalcHalf(double a) {
+    // Hint, try "return a / 2.0;"
+    return a /2.0;
+// WRITE YOUR CODE HERE
+}
+
+int main() {
+    std::cout << CalcHalf(10.0) << std::endl;
+    return 0;
+}
