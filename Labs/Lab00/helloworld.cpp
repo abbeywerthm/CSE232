8359c6ceebe906a5c19436c818a3e33eba439d54
Abigail Werthmann <abbey5048@gmail.com>
2024-08-29 13:34:33 -0400

    adding corrected helloworld.cpp

---
+++ Labs/Lab00/helloworld.cpp
@@ -0,0 +1,15 @@
+/*make sure to do: 
+git add .
+git commit -m "adding __name of file__"
+git push
+so then it will make sure to add to github for it
+to run properly */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
