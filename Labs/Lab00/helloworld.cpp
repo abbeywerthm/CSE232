d39ef70a289379dc855d0d4696331532ea693d45
Abigail Werthmann <abbey5048@gmail.com>
2024-08-29 13:40:00 -0400

    committing new file

--- Labs/Lab00/helloworld.cpp
+++ Labs/Lab00/helloworld.cpp
@@ -1,9 +1,20 @@
-/*make sure to do: 
+/* NOTES:
+always do this:
 git add .
 git commit -m "adding __name of file__"
 git push
+
+AND THEN
+COMMAND + S (to save)
+
 so then it will make sure to add to github for it
 to run properly */
+
+/* How to run terminal:
+g++ -std=c++20 -Wall {your file's name}.cpp
+*/
+
+
 
 
 #include <iostream>
